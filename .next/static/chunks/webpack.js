9a1b919d6bf173437580a2ebe98eaee683936718
Bruno Lima <brunolimadevelopment@gmail.com>
2023-09-25 21:28:30 -0300

    Aula 02: Router

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e8745e89dda635e"; }
+/******/ 		__webpack_require__.h = function() { return "cbfc202ee4106732"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
